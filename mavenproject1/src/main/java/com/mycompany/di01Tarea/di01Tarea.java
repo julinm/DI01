e7599f65b4ff1ed24fed7f881a069ed2be12938e
julinm <82613344+julinm@users.noreply.github.com>
2024-10-29 01:38:29 +0100

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/di01Tarea/di01Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.di01Tarea;
+
+/**
+ *
+ * @author super
+ */
+public class di01Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
